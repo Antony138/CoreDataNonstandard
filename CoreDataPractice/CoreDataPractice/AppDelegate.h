4ac4bce0acd14d90e062cc3fcba77246c9026477
Antony Wong <aeq2005@163.com>
2016-12-13 09:31:09 +0800

    create new project(OC)

---
+++ CoreDataPractice/CoreDataPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataPractice
+//
+//  Created by SPK_Antony on 13/12/2016.
+//  Copyright © 2016 Spark Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreDataPractice/CoreDataPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataPractice
+//
+//  Created by SPK_Antony on 13/12/2016.
+//  Copyright © 2016 Spark Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
