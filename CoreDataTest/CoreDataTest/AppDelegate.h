a54a068e19743f1263e7d90667d9126a477ad3cd
Antony Wong <aeq2005@163.com>
2016-12-13 08:41:28 +0800

    create new project

---
+++ CoreDataTest/CoreDataTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreDataTest
+//
+//  Created by SPK_Antony on 12/12/2016.
+//  Copyright © 2016 Spark Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
