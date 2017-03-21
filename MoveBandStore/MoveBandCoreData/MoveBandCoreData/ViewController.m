be0088a93ba9b440be28a4bd6b2a3c0954a45db7
Antony Wong <aeq2005@163.com>
2017-03-21 17:33:01 +0800

    create "Add" button

--- MoveBandStore/MoveBandCoreData/MoveBandCoreData/ViewController.m
+++ MoveBandStore/MoveBandCoreData/MoveBandCoreData/ViewController.m
@@ -19,6 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
+- (IBAction)addNewUser:(UIButton *)sender {
+    
+}
+
 #pragma mark - UITableViewDataSource
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 3;
