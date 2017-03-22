540b3c53ca474cac1842ba202d21e34e194d691f
Antony Wong <aeq2005@163.com>
2017-03-22 15:40:53 +0800

    create detail view controller

---
+++ MoveBandStore/MoveBandCoreData/MoveBandCoreData/SPKDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPKDetalViewController.h
+//  MoveBandCoreData
+//
+//  Created by SPK_Antony on 22/03/2017.
+//  Copyright © 2017 Spark Technology Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPKDetailViewController : UIViewController
+
+@end
